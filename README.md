939725782024ebe6374c4f867dda1053fa3a3d0a
Christopher Baek <baekc@amazon.com>
2016-12-15 18:24:05 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # javascript-strings-exercises
+
+- http://www.teaching-materials.org/javascript/exercises/strings.html
---
+++ dr-evil.js
@@ -0,0 +1,14 @@
+/*
+
+Dr. Evil
+
+Create a function called DrEvil. It should take a single argument, an
+amount, and return '<amount> dollars', except it will add '(pinky)' at
+the end if the amount is 1 million. For example:
+
+  DrEvil(10): 10 dollars
+  DrEvil(1000000): 1000000 dollars (pinky)
+
+*/
+
+// YOUR CODE GOES HERE
